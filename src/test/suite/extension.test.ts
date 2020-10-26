ca6463419e65dce83a970a6d48f58127c14751f2
Mathias Soeholm <mathiassoeholm@gmail.com>
2020-10-26 21:46:31 +0100

    Start working on a test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,33 @@
-import * as assert from 'assert';
+import * as path from "path";
+import * as fse from "fs-extra";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 // import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+describe("Extension Test Suite", () => {
+  vscode.window.showInformationMessage("Start all tests.");
 
-	test('Sample test', () => {
-		assert.equal(-1, [1, 2, 3].indexOf(5));
-		assert.equal(-1, [1, 2, 3].indexOf(0));
-	});
+  // Copy test folders to temporary location
+  // Open temporary location
+  //
+
+  const testFilesPath = path.join(
+    __dirname,
+    "../../../src/test/suite/test-files"
+  );
+
+  const tempTestFilesPath = path.join(testFilesPath, "../test-files-temp");
+
+  after(() => {
+    fse.removeSync(tempTestFilesPath);
+  });
+
+  it("Sample test", async () => {
+    fse.copySync(testFilesPath, tempTestFilesPath);
+    const uri = vscode.Uri.file(tempTestFilesPath);
+    await vscode.commands.executeCommand("vscode.openFolder", uri);
+    console.log("test");
+  });
 });
