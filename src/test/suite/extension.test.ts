cbe747a2c17c4be7e086eaa2755357ee96213628
Mathias Soeholm <mathiassoeholm@gmail.com>
2020-10-30 19:18:48 +0100

    Start verifying file system in test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,12 @@
 import * as path from "path";
 import * as fse from "fs-extra";
+import * as assert from "assert";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from "vscode";
+import { glob } from "glob";
 // import * as myExtension from '../../extension';
-
-const expectedFileSystem = {
-  ["file-1.md"]: `[link to file-2](new-name.md)`,
-  ["new-name.md"]: `# Just some markdown file`,
-};
 
 interface FileSystemDescription {
   [name: string]: string | FileSystemDescription;
@@ -17,12 +14,38 @@ interface FileSystemDescription {
 
 const tempTestFilesPath = path.resolve("./temp-test-files");
 
-function generateFileSystem(node: string | FileSystemDescription) {
+async function generateFileSystem(node: string | FileSystemDescription) {
   for (const { filePath, fileContent } of collectFilesRecursiveley(node)) {
-    fse.ensureDirSync(path.dirname(filePath));
-    fse.ensureFileSync(filePath);
-    fse.writeFileSync(filePath, fileContent);
+    await fse.ensureFile(filePath);
+    await fse.writeFile(filePath, fileContent);
   }
+}
+
+async function verifyFileSystem(node: string | FileSystemDescription) {
+  const paths = [...collectFilesRecursiveley(node)];
+
+  for (const { filePath, fileContent } of paths) {
+    assert.strictEqual(fileContent, await fse.readFile(filePath, "utf8"));
+  }
+
+  console.log("paths", paths);
+
+  await new Promise((resolve, reject) =>
+    glob(tempTestFilesPath + "/**/*.*", (err, matches) => {
+      if (err) {
+        reject(err);
+      } else {
+        matches.forEach((file) => {
+          console.log("Match", path.normalize(file));
+          assert.strictEqual(
+            true,
+            paths.some(({ filePath }) => filePath === path.normalize(file))
+          );
+        });
+        resolve();
+      }
+    })
+  );
 }
 
 function* collectFilesRecursiveley(
@@ -33,7 +56,7 @@ function* collectFilesRecursiveley(
   fileContent: string;
 }> {
   if (typeof node === "string") {
-    yield { filePath: currentPath, fileContent: node };
+    yield { filePath: path.normalize(currentPath), fileContent: node };
   } else {
     for (const key of Object.keys(node)) {
       yield* collectFilesRecursiveley(node[key], path.join(currentPath, key));
@@ -60,7 +83,14 @@ describe("Extension Test Suite", () => {
       },
     };
 
-    generateFileSystem(startFileSystem);
+    const expectedFileSystem = {
+      ["file-1.md"]: `[link to file-2](folder/new-name.md)`,
+      ["folder"]: {
+        ["new-name.md"]: `# Just some markdown file`,
+      },
+    };
+
+    await generateFileSystem(startFileSystem);
     await vscode.commands.executeCommand(
       "vscode.openFolder",
       vscode.Uri.file(tempTestFilesPath)
@@ -78,5 +108,6 @@ describe("Extension Test Suite", () => {
     console.log("Did edit");
 
     await new Promise((resolve) => setTimeout(resolve, 5000));
+    await verifyFileSystem(expectedFileSystem);
   }).timeout(10000);
 });
