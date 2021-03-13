08a1ab2aacb557141492dee6b4358a2be96203ed
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-03-13 09:28:27 +0100

    Add jest to prepare for making unit tests

---
+++ src/put-get-actions.ts
@@ -0,0 +1,5 @@
+function pureGetActions() {
+  return [];
+}
+
+export { pureGetActions };
---
+++ src/unit-tests/pure-get-actions.test.ts
@@ -0,0 +1,7 @@
+import { pureGetActions } from "../put-get-actions";
+
+describe("pureGetActions", () => {
+  it("works", () => {
+    expect(pureGetActions()).toEqual([]);
+  });
+});
