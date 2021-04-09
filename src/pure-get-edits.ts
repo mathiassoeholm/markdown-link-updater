111f5849240399d6e95246bf1412ac90b336a148
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-04-09 22:20:34 +0200

    Update tests

--- src/pure-get-edits.ts
+++ src/pure-get-edits.ts
@@ -225,7 +225,7 @@ function* handleRenameEvent(
             },
             end: {
               line,
-              character: col + newLink.length,
+              character: col + target.length,
             },
           },
           newText: newLink,
